d83a0fbbe41e10825f22dca39d7a4d646441c04b
Constanza Pizarro <constanza.pizarro.o@ug.uchile.cl>
2023-04-20 18:00:13 -0400

    Adds implementation of drawCard method in Deck class

--- src/main/scala/gwent/Deck.scala
+++ src/main/scala/gwent/Deck.scala
@@ -2,9 +2,17 @@ package cl.uchile.dcc
 package gwent
 /** A class that represents a card deck that each player has. */
 class Deck {
-  private val deck: Set[Card] = Set()
-  /** Draw a card from the deck. */
-  def drawCard(): Unit = {
+  private var deck: Set[Card] = Set()
+  /** Draw a card from the deck.
+   *
+   * @return A card if the deck is not empty.
+   */
+  def drawCard(): Any = {
+    if(deck.nonEmpty) {
+      val card = deck.head
+      deck=deck-card
+      return card
+    }
     // elegir una carta random?
     // o inicializar el mazo, revolver y luego empezar a sacar
   }
