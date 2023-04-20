d41a23ea535769546864dc68f36c76dc0b8e270a
Constanza Pizarro <cony@Constanzas-MacBook.local>
2023-04-20 13:06:39 -0400

    Changes attributes of Deck Class

--- src/main/scala/gwent/Deck.scala
+++ src/main/scala/gwent/Deck.scala
@@ -1,10 +1,11 @@
 package cl.uchile.dcc
 package gwent
 /** A class that represents a card deck that each player has. */
-class Deck(deck: Set[Card]) {
-
+class Deck {
+  private val deck: Set[Card] = Set()
   /** Draw a card from the deck. */
   def drawCard(): Unit = {
     // elegir una carta random?
     // o inicializar el mazo, revolver y luego empezar a sacar
+  }
 }
