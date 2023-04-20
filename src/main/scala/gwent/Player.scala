5065ab5367e0419af35b80c908395ac7b01072ae
Constanza Pizarro <constanza.pizarro.o@ug.uchile.cl>
2023-04-20 18:20:40 -0400

    Changes setCardHand method in Player class

--- src/main/scala/gwent/Player.scala
+++ src/main/scala/gwent/Player.scala
@@ -3,8 +3,14 @@ package gwent
 
 /** A class that represents a player from the game. */
 class Player(private val name: String, private val boardSection: String, private val gemCount: Int,
-             private val cardDeck: Deck, private val cardHand: Set[Card]) {
-
+             private val cardDeck: Deck, private val cardHand: Deck) {
+  /** Set your card hand.
+   *
+   * @param quantity the number of cards you want on your hand.
+   */
+  def setCardHand(quantity: Int): Unit = {
+    cardHand.setCardHand(quantity)
+  }
   /** Place a selected card from your hand on the board.
    *
    * @param card the card you choose to put on the board.
