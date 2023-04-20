9be4ab39672a7f4a10e9224d3160b9bacb283c70
Constanza Pizarro <constanza.pizarro.o@ug.uchile.cl>
2023-04-20 18:32:16 -0400

    Adds playCard method in Card trait

--- src/main/scala/gwent/Card.scala
+++ src/main/scala/gwent/Card.scala
@@ -1,4 +1,10 @@
 package cl.uchile.dcc
 package gwent
 
-trait Card
+trait Card {
+  /** Place a selected card from your hand on the board.
+   *
+   * @param card the card you choose to put on the board.
+   */
+  def playCard(card: Card): Unit
+}
