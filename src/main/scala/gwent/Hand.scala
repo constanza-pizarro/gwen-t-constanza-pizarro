badc4b655777a41ebacbb1d07c895c29d7e01dec
Constanza Pizarro <constanza.pizarro.o@ug.uchile.cl>
2023-04-22 19:55:05 -0400

    Adds setCardHand method in Hand class

--- src/main/scala/gwent/Hand.scala
+++ src/main/scala/gwent/Hand.scala
@@ -2,7 +2,18 @@ package cl.uchile.dcc
 package gwent
 
 import scala.collection.mutable.ListBuffer
-
-class Hand() extends ICardList {
+/** A class that represents a card hand that each player has. */
+class Hand extends ICardList {
   val deck: ListBuffer[ICard] = ListBuffer()
+  /** Set your card hand.
+   *
+   * @param deck the deck you're getting your cards from.
+   * @param quantity the number of cards you want on your hand.
+   */
+  def setCardHand(deck: Deck, quantity: Int): Unit = {
+    for(i <- 0.until(quantity)) {
+      card=Deck.drawCard
+      this.deck+=card
+    }
+  }
 }
