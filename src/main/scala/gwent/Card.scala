4f76d7e3f9ec6062eb57d47aeb5d44fa2554ef28
Constanza Pizarro <constanza.pizarro.o@ug.uchile.cl>
2023-04-22 18:56:57 -0400

    Overrides equals method in Card class

--- src/main/scala/gwent/Card.scala
+++ src/main/scala/gwent/Card.scala
@@ -1,7 +1,15 @@
 package cl.uchile.dcc
 package gwent
 
-class Card(name: String, classification: String) extends ICard {
+class Card(private val name: String, private val classification: String) extends ICard {
   override def playCard(card: Card): Unit = {
+  }
+
+  override def equals(obj: Any): Boolean = {
+    if (obj.isInstanceOf[Card]) {
+      val other = obj.asInstanceOf[Card]
+      (this eq other) || (name == other.name && classification == other.classification)
+    } else
+        false
   }
 }
