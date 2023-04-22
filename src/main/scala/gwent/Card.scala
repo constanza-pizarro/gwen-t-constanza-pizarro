06ed2a6ff7a4e5de61664741e354e17277cb5ef5
Constanza Pizarro <constanza.pizarro.o@ug.uchile.cl>
2023-04-22 19:53:18 -0400

    Adds documentation to equals method

--- src/main/scala/gwent/Card.scala
+++ src/main/scala/gwent/Card.scala
@@ -5,6 +5,10 @@ class Card(private val name: String, private val classification: String) extends
   override def playCard(card: Card): Unit = {
   }
 
+  /** Compare a Card object with an object of any type.
+   *
+   * @param obj object to compare.
+   */
   override def equals(obj: Any): Boolean = {
     if (obj.isInstanceOf[Card]) {
       val other = obj.asInstanceOf[Card]
