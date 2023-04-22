63397b77e7e1740a7fa7a04f6001b809fc7f24b2
Constanza Pizarro <constanza.pizarro.o@ug.uchile.cl>
2023-04-22 19:49:54 -0400

    Changes parameters and drawCard method in Deck class

--- src/main/scala/gwent/Deck.scala
+++ src/main/scala/gwent/Deck.scala
@@ -1,26 +1,22 @@
 package cl.uchile.dcc
 package gwent
+import scala.util.Random
+import scala.collection.mutable.ListBuffer
 /** A class that represents a card deck that each player has. */
-class Deck {
-  private var deck: Set[Card] = Set()
+class Deck() extends ICardList {
+  private val deck: ListBuffer[ICard] = ListBuffer()
+  /** Shuffles card deck.*/
+  def setCardDeck(): Unit = {
+    Random.shuffle(deck)
+  }
   /** Draw a card from the deck.
    *
    * @return A card if the deck is not empty.
    */
-  def drawCard(): Card = {
+  def drawCard(): ICard = {
     val card = deck.head
-    deck=deck-card
+    deck-=card
     return card
     //falta el caso en que está vacío el deck
-  }
-
-  /** Set your card hand.
-   *
-   * @param quantity the number of cards you want on your hand.
-   */
-  def setCardHand(quantity: Int): Unit = {
-    for (w <- 0 to quantity)
-      val card: Card = drawCard()
-      deck=deck+card
   }
 }
