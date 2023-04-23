4e113d188988b1a2a4e5781f12da60cc3d57d898
Constanza Pizarro <constanza.pizarro.o@ug.uchile.cl>
2023-04-23 18:28:27 -0400

    Overrides equals method in AbstractCardSet class

--- src/main/scala/gwent/cards/AbstractCardSet.scala
+++ src/main/scala/gwent/cards/AbstractCardSet.scala
@@ -1,6 +1,19 @@
 package cl.uchile.dcc
-package gwent.cards.classes
+package gwent.cards
 
-class AbstractCardSet {
+import scala.collection.mutable
 
+abstract class AbstractCardSet(val deck: mutable.Set[ICard]) extends ICardSet with Equals {
+  /** Compares a card set object with an object of any type.
+   *
+   * @param that object to compare.
+   */
+  override def equals(that: Any): Boolean = {
+    if (canEqual(that)) {
+      val other = that.asInstanceOf[AbstractCardSet]
+      (this eq other) || (deck == other.deck)
+    } else {
+      false
+    }
+  }  
 }
