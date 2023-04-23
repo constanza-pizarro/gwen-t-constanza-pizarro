97bc29ae997df81257128dc8cb8ba53004ded4d7
Constanza Pizarro <constanza.pizarro.o@ug.uchile.cl>
2023-04-23 19:18:20 -0400

    Modifies setHand method

--- src/main/scala/gwent/cardsets/classes/Hand.scala
+++ src/main/scala/gwent/cardsets/classes/Hand.scala
@@ -1,25 +1,25 @@
 package cl.uchile.dcc
-package gwent.cards
+package gwent.cardsets.classes
 
-import cl.uchile.dcc.gwent.cardsets.AbstractCardSet
+import gwent.cards.ICard
+import gwent.cardsets.AbstractCardSet
+
 import java.util.Objects
 import scala.collection.mutable
 /** A class that represents a card hand that each player has. */
 class Hand(set: mutable.Set[ICard] = mutable.Set()) extends AbstractCardSet(set) {
-  /*
-  def drawCard(): Unit = {
-    //this.deck += cardDeck.drawCard
-  }
-  /** Set your 10-cards hand.
+  
+  //def drawCard(): Unit = this.deck += cardDeck.drawCard
+  /** Set your card hand.
    *
    * @param deck The deck you're getting the cards from.
    */
-  def setCardHand(): Unit = {
-    for(i <- 0.until(10)) {
-      //val card=deck.drawCard
-      this.deck+=card
+  def setHand(deck: Deck, quantity: Int): Unit = {
+    for(i <- 0.until(quantity)) {
+      val card=deck.drawCard()
+      set+=card
     }
-  }*/
+  }
   //DOCUMENTAR!!
   override def canEqual(obj: Any): Boolean = obj.isInstanceOf[Hand]
   /** Compare a Hand object with an object of any type.
