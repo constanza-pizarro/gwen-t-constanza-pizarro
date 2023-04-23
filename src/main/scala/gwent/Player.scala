400d6a085419f9df4ab7d04e04487a24d4260ec2
Constanza Pizarro <constanza.pizarro.o@ug.uchile.cl>
2023-04-22 22:32:24 -0400

    Modifies Player class

--- src/main/scala/gwent/Player.scala
+++ src/main/scala/gwent/Player.scala
@@ -3,25 +3,25 @@ package gwent
 
 /** A class that represents a player from the game. */
 class Player(private val name: String, private val gemCount: Int,
-             private val cardDeck: Deck, private val cardHand: Deck) {
+             val cardDeck: Deck, private val cardHand: Hand) {
   /** Set your card hand.
    *
    * @param quantity the number of cards you want on your hand.
    */
   def setCardHand(quantity: Int): Unit = {
-    cardHand.setCardHand(quantity)
+    cardHand.setCardHand(cardDeck, quantity)
   }
   /** Place a selected card from your hand on the board.
    *
-   * @param card the card you choose to put on the board.
+   * param card the card you choose to put on the board.
    */
-  def playCard(card: Card): Unit = {
+  //def playCard(card: Card): Unit = {
     // posicionarla en el tablero y "eliminarla" de la mano
   }
 
   /** Draw a card from your deck. */
   def drawCard(): Unit = {
-    cardDeck.drawCard()
+    cardDeck.drawCard
   }
 
   /** Compare a Player object with an object of any type.
