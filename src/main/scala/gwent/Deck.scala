8ac18ab2aee59c7b3f9e6d8f68605132ce520f14
Constanza Pizarro <constanza.pizarro.o@ug.uchile.cl>
2023-04-22 22:28:53 -0400

    Overrides equals method in Deck class

--- src/main/scala/gwent/Deck.scala
+++ src/main/scala/gwent/Deck.scala
@@ -3,8 +3,8 @@ package gwent
 import scala.util.Random
 import scala.collection.mutable.ListBuffer
 /** A class that represents a card deck that each player has. */
-class Deck() extends ICardList {
-  private val deck: ListBuffer[ICard] = ListBuffer()
+class Deck extends ICardList {
+  val deck: ListBuffer[ICard] = ListBuffer()
   /** Shuffles card deck.*/
   def setCardDeck(newDeck: ListBuffer[ICard]): Unit = {
     for (c <- newDeck)
@@ -20,5 +20,16 @@ class Deck() extends ICardList {
     deck-=card
     return card
     //falta el caso en que está vacío el deck
+  }
+  /** Compare a Deck object with an object of any type.
+   *
+   * @param obj object to compare.
+   */
+  override def equals(obj: Any): Boolean = {
+    if (obj.isInstanceOf[Deck]) {
+      val other = obj.asInstanceOf[Deck]
+      (this eq other) || (deck == other.deck)
+    } else
+      false
   }
 }
