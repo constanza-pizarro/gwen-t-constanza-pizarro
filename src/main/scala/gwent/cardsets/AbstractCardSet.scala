c4700105b74bf77f8e8dbc9b4e344df966db03a5
Constanza Pizarro <constanza.pizarro.o@ug.uchile.cl>
2023-04-24 00:01:13 -0400

    Adds documentation to AbstractCardSet class and equals method

--- src/main/scala/gwent/cardsets/AbstractCardSet.scala
+++ src/main/scala/gwent/cardsets/AbstractCardSet.scala
@@ -2,17 +2,23 @@ package cl.uchile.dcc
 package gwent.cardsets
 
 import gwent.cards.ICard
-import gwent.cardsets.ICardSet
-
 import scala.collection.mutable
 
-/** Represents a generic card in the game. Not to be instanced
+/** A class representing a generic card set in the game, not to be instanced.
  *
- * @param set A set of cards */
+ * This card set is defined by its set of cards and it can contain both weather and unit cards.
+ *
+ * @param set The set of cards.
+ *
+ * @constructor Creates a new card set with the specified set of cards.
+ *
+ * @author Constanza Pizarro
+ */
 abstract class AbstractCardSet(val set: mutable.Set[ICard]) extends ICardSet with Equals {
-  /** Compares a card set object with an object of any type.
+  /** Compares a card set with an object of any type.
    *
-   * @param that object to compare.
+   * @param that object to compare with this instance.
+   * @return true if the object and this instance are equal (structurally or referentially).
    */
   override def equals(that: Any): Boolean = {
     if (canEqual(that)) {
