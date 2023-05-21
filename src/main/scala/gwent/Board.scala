87955b0996a207cae53a82d8a89aa73f870fe05f
Constanza Pizarro <constanza.pizarro.o@ug.uchile.cl>
2023-05-21 12:40:06 -0400

    FEAT: Adds Board class

---
+++ src/main/scala/gwent/Board.scala
@@ -0,0 +1,24 @@
+package cl.uchile.dcc
+package gwent.board
+
+import gwent.Player
+import gwent.cards.Card
+/** Class representing a board in the Gwen't game.
+ *
+ * The boards has 2 symmetric sections, one for each player, each section is divided by
+ * 3 zones or rows. The board also has a weather zone, shared by the players.
+ *
+ * @constructor Create a new board with 2 players.
+ * @param player1 The player one of the game
+ * @param player2 The player two of the game
+ *
+ * @author Constanza Pizarro
+ */
+class Board(val player1: Player, val player2: Player) {
+  //considerando que cada jugador tiene zona de cc, rc, sc y weather
+  
+  
+  
+  
+
+}
