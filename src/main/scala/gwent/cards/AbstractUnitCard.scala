5aeb420e67dbeed6891bda3ce02d5f878f8de89c
Constanza Pizarro <constanza.pizarro.o@ug.uchile.cl>
2023-05-21 00:56:38 -0400

    FEAT: Replaces UnitCard class with AbstractUnitCard abstract class and CloseCombatCard, RangedCombatCard and SiegeCombatCard classes

---
+++ src/main/scala/gwent/cards/AbstractUnitCard.scala
@@ -0,0 +1,36 @@
+package cl.uchile.dcc
+package gwent.cards
+
+import java.util.Objects
+/** Abstract class representing a unit card in the Gwen't game.
+ *
+ * An `AbstractUnitCard` is a type of [[Card]] that has a power value which contributes to
+ * the player's total power in the game.
+ * Each unit card starts with its current power equal to its base power.
+ *
+ * @constructor Creates a new `AbstractUnitCard` with a specified name, description, and
+ *              power.
+ * @param name The name of the card.
+ * @param description The description of the card, explaining its specific abilities or
+ *                    role in the game.
+ * @param power The base power of the card, indicating the contribution of this card to
+ *              the player's total power when unaffected by any special conditions.
+ *
+ * @author <a href="https://www.github.com/r8vnhill">R8V</a>
+ * @author Constanza Pizarro
+ */
+abstract class AbstractUnitCard protected(val name: String, val description: String,
+                                          val power: Int) extends Card {
+  /** The current power of the card, which may be affected by various conditions during
+   * gameplay.
+   * Initially set to the base [[power]] of the card.
+   */
+  var currentPower: Int = power
+  override def equals(obj: Any): Boolean = obj match {
+    case other: AbstractUnitCard =>
+      (this eq other) ||
+        name == other.name && description == other.description && power == other.power
+    case _ =>
+      false
+  }
+}
---
+++ src/main/scala/gwent/cards/CloseCombatCard.scala
@@ -0,0 +1,34 @@
+package cl.uchile.dcc
+package gwent.cards
+
+import java.util.Objects
+
+/** Class representing a close combat unit card in the Gwen't game.
+ *
+ * A `CloseCombatCard` is a type of [[AbstractUnitCard]].
+ * These cards represent troops that engage in close combat on the battlefield.
+ * As per the game's rules, these cards can only be placed in the close combat zone.
+ *
+ * @constructor Creates a new `CloseCombatCard` with a specified name, description, and
+ *              power.
+ * @param name the name of the card.
+ * @param description the description of the card.
+ * @param power the initial power value of the card, which also corresponds to the
+ *              strength of the troop it represents.
+ *              
+ * @author <a href="https://www.github.com/r8vnhill">R8V</a>
+ * @author Constanza Pizarro
+ */
+class CloseCombatCard(name: String, description: String, power: Int)
+  extends AbstractUnitCard(name, description, power) {
+  override def equals(obj: Any): Boolean = obj match {
+    case other: CloseCombatCard =>
+      super.equals(other)
+    case _ =>
+      false
+  }
+  override def hashCode(): Int =
+    Objects.hash(classOf[CloseCombatCard], name, description, power)
+  override def toString =
+    s"CloseCombatCard(name: $name, description: $description, power: $power)"
+}
