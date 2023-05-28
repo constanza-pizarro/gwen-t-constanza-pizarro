68d0af4b5feede8d273de1612aa7c6270e14d04f
Constanza Pizarro <constanza.pizarro.o@ug.uchile.cl>
2023-05-28 17:43:18 -0400

    FEAT: Overrides equals and hashCode method in Section

--- src/main/scala/gwent/Section.scala
+++ src/main/scala/gwent/Section.scala
@@ -2,6 +2,7 @@ package cl.uchile.dcc
 package gwent
 
 import gwent.cards.Card
+import java.util.Objects
 
 /** Class representing a section in the Gwen't game.
  *
@@ -15,6 +16,17 @@ import gwent.cards.Card
  *
  * @author Constanza Pizarro
  */
-class Section(var board: Board, var closeCombatZone: List[Card] = List(),
+class Section(val board: Board, var closeCombatZone: List[Card] = List(),
               var rangedCombatZone: List[Card] = List(),
-              var siegeCombatZone: List[Card] = List())
+              var siegeCombatZone: List[Card] = List()) {
+  override def equals(obj: Any): Boolean = obj match {
+    case other: Section =>
+      (this eq other) || (board == other.board
+        && closeCombatZone == other.closeCombatZone
+        && rangedCombatZone == other.rangedCombatZone
+        && siegeCombatZone == other.siegeCombatZone)
+    case _ => false
+  }
+  override def hashCode(): Int =
+    Objects.hash(classOf[Section], board, closeCombatZone, rangedCombatZone, siegeCombatZone)
+}
