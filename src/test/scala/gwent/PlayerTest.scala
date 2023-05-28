5b7b944a410b9ef5a6e0e2728557816fc412c88e
Constanza Pizarro <constanza.pizarro.o@ug.uchile.cl>
2023-05-28 17:16:31 -0400

    TEST: Modifies tests in PlayerTest

--- src/test/scala/gwent/PlayerTest.scala
+++ src/test/scala/gwent/PlayerTest.scala
@@ -21,35 +21,33 @@ class PlayerTest extends munit.FunSuite {
   val wc2 = new WeatherCard("Impenetrable Fog",
     "Sets the strength of all Ranged Combat cards to 1 for both players.")
 
-  val d1: List[Card] = List(cc1, cc2, rc1, rc2, sc1, wc1)
-  val d2: List[Card] = List(cc2, rc2, rc1, sc2, sc1, wc2)
+  val d2: List[Card] = List(cc2, rc2, sc2, wc2)
 
-  val h1: List[Card] = List(sc2, wc2)
-  val h2: List[Card] = List(cc1, wc1, rc1)
+  val h2: List[Card] = List(cc1, wc1, rc1, sc1)
 
   var p1: Player = _
   var p2: Player = _
 
   var board: Board = _
 
-  var section1: Section = new Section(board)
-  var section2: Section = new Section(board)
+  val s1: Section = new Section(board)
 
   override def beforeEach(context: BeforeEach): Unit = {
-    p1 = new Player("player1", section1,2, d1, h1)
-    p2 = new Player("player2", section2, 2, d2, h2)
+    p1 = new Player("player1", s1,2)
+    p2 = new Player("player2", s1, 2, d2, h2)
     board = new Board(p1, p2)
   }
 
-  test("well defined player") {
+  test("well defined player, deck and hand") {
     assertEquals(p1.name, "player1")
-    assertEquals(p1.section, section1)
+    assertEquals(p1.section, s1)
     assertEquals(p1.gemCounter, 2)
-    assertEquals(p1.deck, d1)
-    assertEquals(p1.hand, h1)
+    assertEquals(p1.deck, List())
+    assertEquals(p2.deck, d2)
+    assertEquals(p1.hand, List())
+    assertEquals(p2.hand, h2)
 
     assertEquals(p1.section.board, p2.section.board)
-    p1.shuffleDeck()
     p2.shuffleDeck()
     p2.deck == d2
 
@@ -60,18 +58,19 @@ class PlayerTest extends munit.FunSuite {
     assertEquals(c1 :: h2, p2.hand)
     println(d2)
   }
+
   test("play card") {
-    p1.playCard(sc2)
-    p1.playCard(wc2)
     p2.playCard(cc1)
+    p2.playCard(wc1)
     p2.playCard(rc1)
-    p1.playCard(cc1) //tira exception porque la carta no está en la mano
+    p2.playCard(sc1)
   }
+
   test("equals") {
     assertEquals(p1, p1)
     assertEquals(p2, p2)
-    //assertEquals(p1, new Player("player1", 2, d1, h1, board))
-    //assertEquals(p2, new Player("player2", 2, d2, h2, board))
+    assertEquals(p1, new Player("player1", s1,2))
+    assertEquals(p2, new Player("player2", s1,2, d2, h2))
 
     assert(!p1.equals(cc1))
   }
