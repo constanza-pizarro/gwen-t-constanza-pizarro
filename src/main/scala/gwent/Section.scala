99bfa4ef76ac70aedd16dbd2f8bd92b98ffdcc8b
Constanza Pizarro <constanza.pizarro.o@ug.uchile.cl>
2023-05-28 15:17:58 -0400

    DOCS: Adds documentation to Section class

--- src/main/scala/gwent/Section.scala
+++ src/main/scala/gwent/Section.scala
@@ -3,6 +3,18 @@ package gwent
 
 import gwent.cards.Card
 
+/** Class representing a section in the Gwen't game.
+ *
+ * Each section belongs to a board and has Close, Ranged and Siege combat zones.
+ *
+ * @constructor Create a new section with a board and Close, Ranged and Siege combat zones
+ * @param board The board the section belongs to.
+ * @param closeCombatZone The list of cards in the close combat zone.
+ * @param rangedCombatZone The list of cards in the ranged combat zone.
+ * @param siegeCombatZone The list of cards in the siege combat zone.
+ *
+ * @author Constanza Pizarro
+ */
 class Section(var board: Board, var closeCombatZone: List[Card] = List(),
               var rangedCombatZone: List[Card] = List(),
               var siegeCombatZone: List[Card] = List())
