92112d6740d337cfeeabce168a645160de5878db
Constanza Pizarro <constanza.pizarro.o@ug.uchile.cl>
2023-05-28 15:03:33 -0400

    FEAT: Modifies parameters of Section class constructor

--- src/main/scala/gwent/Section.scala
+++ src/main/scala/gwent/Section.scala
@@ -3,6 +3,6 @@ package gwent
 
 import gwent.cards.Card
 
-class Section(var closeCombatZone: List[Card] = List(),
+class Section(var board: Board, var closeCombatZone: List[Card] = List(),
               var rangedCombatZone: List[Card] = List(),
               var siegeCombatZone: List[Card] = List())
