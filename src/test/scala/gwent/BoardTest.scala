2e1f8136d96a81ea60bf886798ba5e3845ce2264
Constanza Pizarro <constanza.pizarro.o@ug.uchile.cl>
2023-05-28 16:39:23 -0400

    TEST: Adds BoardTest class

--- src/test/scala/gwent/BoardTest.scala
+++ src/test/scala/gwent/BoardTest.scala
@@ -1,5 +1,26 @@
-package cl.uchile.dcc.gwent
+package cl.uchile.dcc
+package gwent
 
-class BoardTest {
+import gwent.cards.*
 
+class BoardTest extends munit.FunSuite {
+  var p1: Player = _
+  var p2: Player = _
+
+  var board: Board = _
+
+  val section1: Section = new Section(board)
+  val section2: Section = new Section(board)
+
+  override def beforeEach(context: BeforeEach): Unit = {
+    p1 = new Player("player1", section1, 2)
+    p2 = new Player("player2", section2, 2)
+    board = new Board(p1, p2)
+  }
+
+  test("well defined board") {
+    assertEquals(board.player1, p1)
+    assertEquals(board.player2, p2)
+    assertEquals(board.weatherZone, List())
+  }
 }
