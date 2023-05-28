1fc2300793b9937fc565a8729e8ce4f39efb2748
Constanza Pizarro <constanza.pizarro.o@ug.uchile.cl>
2023-05-28 14:23:40 -0400

    FEAT: Implements playUnitCard method in SiegeCombatCard class

--- src/main/scala/gwent/cards/SiegeCombatCard.scala
+++ src/main/scala/gwent/cards/SiegeCombatCard.scala
@@ -1,7 +1,7 @@
 package cl.uchile.dcc
 package gwent.cards
 
-import cl.uchile.dcc.gwent.{Board, Player}
+import cl.uchile.dcc.gwent.Player
 import java.util.Objects
 
 /** Class representing a siege combat unit card in the Gwen't game.
@@ -21,10 +21,10 @@ import java.util.Objects
  */
 class SiegeCombatCard(name: String, description: String, power: Int)
   extends AbstractUnitCard(name, description, power) {
-  override def playCard(board: Board, player: Player): Unit = {
-    require(player.hand.nonEmpty, "the player's hand must not be empty")
-    val zone = player.siegeCombatZone
-    player.siegeCombatZone = this :: zone
+  override def playUnitCard(player: Player): Unit = {
+    super.playUnitCard(player)
+    val zone = player.section.siegeCombatZone
+    player.section.siegeCombatZone = this :: zone
   }
   override def equals(obj: Any): Boolean = obj match {
     case other: SiegeCombatCard =>
