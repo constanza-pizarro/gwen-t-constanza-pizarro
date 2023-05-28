de172c6f953d305d14f15e8cbd159a20d4e6ab31
Constanza Pizarro <constanza.pizarro.o@ug.uchile.cl>
2023-05-28 19:17:58 -0400

    FEAT: Adds playCloseCombatCard, playRangedCombatCard and playSiegeCombatCard methods in Section

--- src/main/scala/gwent/Section.scala
+++ src/main/scala/gwent/Section.scala
@@ -1,32 +1,39 @@
 package cl.uchile.dcc
 package gwent
 
-import gwent.cards.Card
+import gwent.cards.*
 import java.util.Objects
 
 /** Class representing a section in the Gwen't game.
  *
- * Each section belongs to a board and has Close, Ranged and Siege combat zones.
+ * Each section has Close, Ranged and Siege combat zones.
  *
- * @constructor Create a new section with a board and Close, Ranged and Siege combat zones
- * @param board The board the section belongs to.
+ * @constructor Create a new section with Close, Ranged and Siege combat zones
  * @param closeCombatZone The list of cards in the close combat zone.
  * @param rangedCombatZone The list of cards in the ranged combat zone.
  * @param siegeCombatZone The list of cards in the siege combat zone.
  *
  * @author Constanza Pizarro
  */
-class Section(val board: Board, var closeCombatZone: List[Card] = List(),
+class Section(var closeCombatZone: List[Card] = List(),
               var rangedCombatZone: List[Card] = List(),
               var siegeCombatZone: List[Card] = List()) {
+  def playCloseCombatCard(ccCard: CloseCombatCard): Unit = {
+    closeCombatZone = ccCard :: closeCombatZone
+  }
+  def playRangedCombatCard(rcCard: RangedCombatCard): Unit = {
+    rangedCombatZone = rcCard :: rangedCombatZone
+  }
+  def playSiegeCombatCard(scCard: SiegeCombatCard): Unit = {
+    siegeCombatZone = scCard :: siegeCombatZone
+  }
   override def equals(obj: Any): Boolean = obj match {
     case other: Section =>
-      (this eq other) || (board == other.board
-        && closeCombatZone == other.closeCombatZone
+      (this eq other) || (closeCombatZone == other.closeCombatZone
         && rangedCombatZone == other.rangedCombatZone
         && siegeCombatZone == other.siegeCombatZone)
     case _ => false
   }
   override def hashCode(): Int =
-    Objects.hash(classOf[Section], board, closeCombatZone, rangedCombatZone, siegeCombatZone)
+    Objects.hash(classOf[Section], closeCombatZone, rangedCombatZone, siegeCombatZone)
 }
