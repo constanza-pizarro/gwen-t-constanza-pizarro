db9dede706c3611a9528b6a6033faf1acb3e0074
Constanza Pizarro <constanza.pizarro.o@ug.uchile.cl>
2023-06-06 22:41:38 -0400

    FEAT: Adds endTurn method in GameState

--- src/main/scala/gwent/controller/states/GameState.scala
+++ src/main/scala/gwent/controller/states/GameState.scala
@@ -10,8 +10,10 @@ class GameState(var context: GameController) {
   def startGame(): Unit = {
     transitionError("TurnState")
   }
-  /** Does nothing. */
+  /** Changes the current player. */
   def playCard(): Unit = {}
+  /** Ends the turn for the current player. */
+  def endTurn(): Unit = {}
   private def transitionError(targetState: String): Unit = {
     throw new InvalidTransitionException(
       s"Cannot transition from ${getClass.getSimpleName} to $targetState"
