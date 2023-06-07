613ede21f4171e4a8e9dbc4e9d869d0b3daa3092
Constanza Pizarro <constanza.pizarro.o@ug.uchile.cl>
2023-06-06 22:21:50 -0400

    FEAT: Adds startGame and transitionError methods in GameState

--- src/main/scala/gwent/controller/states/GameState.scala
+++ src/main/scala/gwent/controller/states/GameState.scala
@@ -1,6 +1,17 @@
 package cl.uchile.dcc
 package gwent.controller.states
 
-class GameState {
+import gwent.controller.GameController
+import gwent.cards.Card
+import gwent.Player
 
+class GameState(var context: GameController) {
+  def startGame(): Unit = {
+    transitionError("TurnState")
+  }
+  private def transitionError(targetState: String): Unit = {
+    throw new InvalidTransitionException(
+      s"Cannot transition from ${getClass.getSimpleName} to $targetState"
+    )
+  }
 }
