697420dcd41c59559e89e03e1a9e5ff9f418009e
Constanza Pizarro <constanza.pizarro.o@ug.uchile.cl>
2023-06-06 22:42:22 -0400

    FEAT: Overrides endTurn method in TurnState

--- src/main/scala/gwent/controller/states/TurnState.scala
+++ src/main/scala/gwent/controller/states/TurnState.scala
@@ -4,8 +4,10 @@ package gwent.controller.states
 import gwent.controller.GameController
 
 class TurnState(controller: GameController) extends GameState(controller) {
-  /** Gives the turn to the other player. */
   override def playCard(): Unit = {
     controller.changePlayer()
+  }
+  override def endTurn(): Unit = {
+    controller.state = new AloneState(controller)
   }
 }
