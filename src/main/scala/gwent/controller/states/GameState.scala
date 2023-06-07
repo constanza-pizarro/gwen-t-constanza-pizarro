fe1416e777a7bc7c9f42d04348d4da1d88f17cdf
Constanza Pizarro <constanza.pizarro.o@ug.uchile.cl>
2023-06-06 22:31:38 -0400

    FEAT: Implements playCard method in GameState

--- src/main/scala/gwent/controller/states/GameState.scala
+++ src/main/scala/gwent/controller/states/GameState.scala
@@ -6,9 +6,12 @@ import gwent.cards.Card
 import gwent.Player
 
 class GameState(var context: GameController) {
+  /** Transitions to TurnState. */
   def startGame(): Unit = {
     transitionError("TurnState")
   }
+  /** Does nothing. */
+  def playCard(): Unit = {}
   private def transitionError(targetState: String): Unit = {
     throw new InvalidTransitionException(
       s"Cannot transition from ${getClass.getSimpleName} to $targetState"
