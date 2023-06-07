661e84e26ab42c5b84e9aa5b9ecc59a6f5cdea64
Constanza Pizarro <constanza.pizarro.o@ug.uchile.cl>
2023-06-06 22:32:02 -0400

    FEAT: Overrides playCard method in TurnState

--- src/main/scala/gwent/controller/states/TurnState.scala
+++ src/main/scala/gwent/controller/states/TurnState.scala
@@ -1,5 +1,11 @@
-package cl.uchile.dcc.gwent.controller.states
+package cl.uchile.dcc
+package gwent.controller.states
 
-class TurnState {
+import gwent.controller.GameController
 
+class TurnState(controller: GameController) extends GameState(controller) {
+  /** Gives the turn to the other player. */
+  override def playCard(): Unit = {
+    controller.changePlayer()
+  }
 }
