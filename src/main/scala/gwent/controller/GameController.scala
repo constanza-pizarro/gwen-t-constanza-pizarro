8b73130a1fdfc66607f942014c437dfddc42aca9
Constanza Pizarro <constanza.pizarro.o@ug.uchile.cl>
2023-06-06 21:36:45 -0400

    FEAT: Adds startGame method to GameController class

--- src/main/scala/gwent/controller/GameController.scala
+++ src/main/scala/gwent/controller/GameController.scala
@@ -1,6 +1,14 @@
 package cl.uchile.dcc
 package gwent.controller
 
-class GameController {
+import gwent.controller.states.*
 
+class GameController {
+  var state: GameState = new StartState(this)
+  def startGame(): Unit = {
+    println("Player 1 name?")
+    val name1: String = scala.io.StdIn.readLine()
+    println("Player 2 name?")
+    val name2: String = scala.io.StdIn.readLine()
+  }
 }
