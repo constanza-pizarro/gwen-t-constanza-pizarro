1358e961ae7196a290995a2c2848cc4b22306c4a
Constanza Pizarro <constanza.pizarro.o@ug.uchile.cl>
2023-06-07 22:34:01 -0400

    FEAT: Adds NewRoundState class in controller.states package

---
+++ src/main/scala/gwent/controller/states/NewRoundState.scala
@@ -0,0 +1,10 @@
+package cl.uchile.dcc
+package gwent.controller.states
+
+import gwent.controller.GameController
+
+import cl.uchile.dcc.gwent.Player
+
+class NewRoundState(controller: GameController) extends GameState(controller) {
+
+}
