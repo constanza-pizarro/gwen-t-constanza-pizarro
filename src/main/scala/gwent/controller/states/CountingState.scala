9325bb6e7c89582f8f9c988c5cada26d7e28488d
Constanza Pizarro <constanza.pizarro.o@ug.uchile.cl>
2023-06-07 22:44:48 -0400

    FEAT: Adds CountingState class in controller.states package

---
+++ src/main/scala/gwent/controller/states/CountingState.scala
@@ -0,0 +1,10 @@
+package cl.uchile.dcc
+package gwent.controller.states
+
+import gwent.controller.GameController
+
+import cl.uchile.dcc.gwent.Player
+
+class CountingState(controller: GameController) extends GameState(controller) {
+
+}
