577991ceafcca9fc0c37a2b26a2d25947328caeb
Constanza Pizarro <constanza.pizarro.o@ug.uchile.cl>
2023-06-07 23:07:37 -0400

    FEAT: Overrides newRound method in CountState

--- src/main/scala/gwent/controller/states/CountState.scala
+++ src/main/scala/gwent/controller/states/CountState.scala
@@ -6,5 +6,8 @@ import gwent.controller.GameController
 import cl.uchile.dcc.gwent.Player
 
 class CountState(controller: GameController) extends GameState(controller) {
+  override def newRound(): Unit = {
+    controller.state = new RoundState(controller)
+  }
   override def isInCount(): Boolean = true
 }
