53dc20354cd44e4137c84a81b09a8dd15185e635
Constanza Pizarro <constanza.pizarro.o@ug.uchile.cl>
2023-06-07 23:11:27 -0400

    FEAT: Overrides startRound method in RoundState

--- src/main/scala/gwent/controller/states/RoundState.scala
+++ src/main/scala/gwent/controller/states/RoundState.scala
@@ -6,5 +6,8 @@ import gwent.controller.GameController
 import cl.uchile.dcc.gwent.Player
 
 class RoundState(controller: GameController) extends GameState(controller) {
+  override def startRound(): Unit = {
+    controller.state = new TurnState(controller)
+  }
   override def isInRound(): Boolean = true
 }
