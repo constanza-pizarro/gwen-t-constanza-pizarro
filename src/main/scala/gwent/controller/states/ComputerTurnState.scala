0d7c6c8106cb7a62b3696bb4b4bcd24518ec5aeb
Constanza Pizarro <constanza.pizarro.o@ug.uchile.cl>
2023-06-07 22:54:20 -0400

    REFACTOR: Deletes ComputerTurnState

--- src/main/scala/gwent/controller/states/ComputerTurnState.scala
+++
@@ -1,8 +0,0 @@
-package cl.uchile.dcc
-package gwent.controller.states
-
-import gwent.controller.GameController
-
-class ComputerTurnState(controller: GameController) extends GameState(controller) {
-
-}
