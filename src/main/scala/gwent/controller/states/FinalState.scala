b788626c93542a411c1b2644e3ca26009767618f
Constanza Pizarro <constanza.pizarro.o@ug.uchile.cl>
2023-06-07 22:46:32 -0400

    FEAT: Adds FinalState class in controller.states package

---
+++ src/main/scala/gwent/controller/states/FinalState.scala
@@ -0,0 +1,8 @@
+package cl.uchile.dcc
+package gwent.controller.states
+
+import gwent.controller.GameController
+
+class FinalState(controller: GameController) extends GameState(controller) {
+  
+}
