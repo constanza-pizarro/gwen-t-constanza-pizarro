72fce6bbc42a8c99d6249370f7ca47bc89991190
Constanza Pizarro <constanza.pizarro.o@ug.uchile.cl>
2023-06-07 22:50:43 -0400

    FEAT: Overrides isInAlone method in AloneState

--- src/main/scala/gwent/controller/states/AloneState.scala
+++ src/main/scala/gwent/controller/states/AloneState.scala
@@ -4,5 +4,5 @@ package gwent.controller.states
 import gwent.controller.GameController
 
 class AloneState(controller: GameController) extends GameState(controller) {
-
+  override def isInAlone(): Boolean = true
 }
