a6cab1d600bc8349f65262f10ec7388001102e42
Constanza Pizarro <constanza.pizarro.o@ug.uchile.cl>
2023-06-07 22:52:10 -0400

    FEAT: Overrides isInTurn method in TurnState

--- src/main/scala/gwent/controller/states/TurnState.scala
+++ src/main/scala/gwent/controller/states/TurnState.scala
@@ -4,10 +4,8 @@ package gwent.controller.states
 import gwent.controller.GameController
 
 class TurnState(controller: GameController) extends GameState(controller) {
-  override def playCard(): Unit = {
-    controller.changePlayer()
-  }
   override def endTurn(): Unit = {
     controller.state = new AloneState(controller)
   }
+  override def isInTurn(): Boolean = true
 }
