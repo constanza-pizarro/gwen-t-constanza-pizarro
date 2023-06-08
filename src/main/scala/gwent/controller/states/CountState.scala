0a639957556993689393b6d77c31bd9dbd82000b
Constanza Pizarro <constanza.pizarro.o@ug.uchile.cl>
2023-06-07 23:05:29 -0400

    FEAT: Overrides isInCount method in CountState

--- src/main/scala/gwent/controller/states/CountState.scala
+++ src/main/scala/gwent/controller/states/CountState.scala
@@ -6,5 +6,5 @@ import gwent.controller.GameController
 import cl.uchile.dcc.gwent.Player
 
 class CountState(controller: GameController) extends GameState(controller) {
-
+  override def isInCount(): Boolean = true
 }
