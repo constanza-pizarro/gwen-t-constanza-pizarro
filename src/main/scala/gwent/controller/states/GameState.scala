46866a6ad36718b39fb455e3cd58e448c5258f09
Constanza Pizarro <constanza.pizarro.o@ug.uchile.cl>
2023-06-07 23:03:13 -0400

    FEAT: Adds isInCount method in GameState

--- src/main/scala/gwent/controller/states/GameState.scala
+++ src/main/scala/gwent/controller/states/GameState.scala
@@ -26,6 +26,7 @@ class GameState(var context: GameController) {
   def isInStart(): Boolean = false
   def isInTurn(): Boolean = false
   def isInAlone(): Boolean = false
+  def isInCount(): Boolean = false
   def isInRound(): Boolean = false
   def isInFinal(): Boolean = false
   private def transitionError(targetState: String): Unit = {
