72af8595bf2948786055a8aaf6ca1c97f0111bad
Constanza Pizarro <constanza.pizarro.o@ug.uchile.cl>
2023-06-23 00:23:00 -0400

    FEAT: Adds ClearWeather case class

---
+++ src/main/scala/gwent/cards/effects/weather/ClearWeather.scala
@@ -0,0 +1,10 @@
+package cl.uchile.dcc
+package gwent.cards.effects.weather
+
+import gwent.cards.effects.WeatherEffect
+
+case class ClearWeather() extends WeatherEffect {
+  override def apply(): Unit = {
+
+  }
+}
