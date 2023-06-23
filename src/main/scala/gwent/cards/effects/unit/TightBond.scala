f7c93e9eca063c7c08274c86600e99353ba74a17
Constanza Pizarro <constanza.pizarro.o@ug.uchile.cl>
2023-06-23 00:17:56 -0400

    REFACTOR: TightBond is now a case class

--- src/main/scala/gwent/cards/effects/unit/TightBond.scala
+++ src/main/scala/gwent/cards/effects/unit/TightBond.scala
@@ -4,7 +4,7 @@ package gwent.cards.effects.unit
 import gwent.cards.effects.UnitEffect
 import gwent.cards.UnitCard
 
-class TightBond extends UnitEffect {
+case class TightBond() extends UnitEffect {
   def apply(self: UnitCard, target: List[UnitCard]): Unit = {
     val nTarget = target.filter(_.equals(self))
     if (nTarget.nonEmpty) {
