c6f6fc5c2e18ec7b0fba819fc6e6801fd169141c
Constanza Pizarro <constanza.pizarro.o@ug.uchile.cl>
2023-06-23 00:17:21 -0400

    REFACTOR: MoraleBoost is now a case class

--- src/main/scala/gwent/cards/effects/unit/MoraleBoost.scala
+++ src/main/scala/gwent/cards/effects/unit/MoraleBoost.scala
@@ -4,10 +4,9 @@ package gwent.cards.effects.unit
 import gwent.cards.effects.UnitEffect
 import gwent.cards.UnitCard
 
-class MoraleBoost extends UnitEffect {
+case class MoraleBoost() extends UnitEffect {
   def apply(self: UnitCard, target: List[UnitCard]): Unit = {
     var nTarget = target.filterNot(_ eq self)
     nTarget.foreach(i => i.currentPower += 1)
   }
-
 }
