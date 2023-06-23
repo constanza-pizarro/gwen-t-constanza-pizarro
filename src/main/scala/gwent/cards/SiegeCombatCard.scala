adc7aff7b6a3dbbdc502dc7814e186bb677158b3
Constanza Pizarro <constanza.pizarro.o@ug.uchile.cl>
2023-06-23 00:42:49 -0400

    REFACTOR: Modifies effect parameter in SiegeCombatCard

--- src/main/scala/gwent/cards/SiegeCombatCard.scala
+++ src/main/scala/gwent/cards/SiegeCombatCard.scala
@@ -2,8 +2,7 @@ package cl.uchile.dcc
 package gwent.cards
 
 import gwent.Section
-
-import cl.uchile.dcc.gwent.cards.effects.Effect
+import gwent.cards.effects.UnitEffect
 
 import java.util.Objects
 
@@ -22,8 +21,8 @@ import java.util.Objects
  * @author <a href="https://www.github.com/r8vnhill">R8V</a>
  * @author Constanza Pizarro
  */
-class SiegeCombatCard(name: String, description: String, power: Int, effect: Effect)
-  extends AbstractUnitCard(name, description, power, effect) {
+class SiegeCombatCard(name: String, effect: UnitEffect, description: String, power: Int)
+  extends AbstractUnitCard(name, effect, description, power) {
   override def playUnitCard(section: Section): Unit =
     section.playSiegeCombatCard(this)
   override def canEqual(that: Any): Boolean = 
