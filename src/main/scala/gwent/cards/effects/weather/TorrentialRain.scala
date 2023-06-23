654224a22c880cb7c8be7ef5cb401a44d2ce6907
Constanza Pizarro <constanza.pizarro.o@ug.uchile.cl>
2023-06-23 00:21:52 -0400

    FEAT: Adds TorrentialRain case class

---
+++ src/main/scala/gwent/cards/effects/weather/TorrentialRain.scala
@@ -0,0 +1,10 @@
+package cl.uchile.dcc
+package gwent.cards.effects.weather
+
+import gwent.cards.effects.WeatherEffect
+
+case class TorrentialRain() extends WeatherEffect {
+  def apply(): Unit = {
+
+  }
+}
