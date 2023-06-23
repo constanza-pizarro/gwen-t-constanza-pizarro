c2e46c9c55890e0f21eca4eaec3542802fcddb2b
Constanza Pizarro <constanza.pizarro.o@ug.uchile.cl>
2023-06-23 14:55:33 -0400

    TEST: Modifies tests in PlayerTest

--- src/test/scala/gwent/PlayerTest.scala
+++ src/test/scala/gwent/PlayerTest.scala
@@ -7,6 +7,9 @@ import gwent.cards.effects.weather.*
 
 import org.junit.Assert
 
+import scala.+:
+import scala.collection.mutable.ListBuffer
+
 class PlayerTest extends munit.FunSuite {
   val mBoost = "Adds +1 to all units in the row (excluding itself)."
   val tBond = "When placed with the same card, doubles the strength of both (or more) cards"
@@ -25,18 +28,18 @@ class PlayerTest extends munit.FunSuite {
   val wc2 = new WeatherCard("Impenetrable Fog", ImpenetrableFog(),
     "Sets the strength of all Ranged Combat cards to 1 for both players.")
 
-  val d1: List[Card] = List(cc1, rc1, wc1)
-  val d2: List[Card] = List(cc2, rc2, sc2, wc2)
-  val h2: List[Card] = List(cc1, wc1, rc1, sc1)
+  val d1: ListBuffer[Card] = ListBuffer(cc1, rc1, wc1)
+  val d2: ListBuffer[Card] = ListBuffer(cc2, rc2, sc2, wc2)
+  val h2: ListBuffer[Card] = ListBuffer(cc1, wc1, rc1, sc1)
 
   var p1: Player = _
   var p2: Player = _
   var board: Board = _
-  var s1: Section = new Section()
+  var s1: Section = new Section
 
   override def beforeEach(context: BeforeEach): Unit = {
     p1 = new Player("player1")
-    p2 = new Player("player2", _deck = d2, _hand = h2)
+    p2 = new Player("player2", d2, ListBuffer(cc1, wc1, rc1, sc1))
     board = new Board(p1, p2)
   }
 
@@ -49,27 +52,26 @@ class PlayerTest extends munit.FunSuite {
     p1.loseGem()
     val e1 = Assert.assertThrows(classOf[IllegalArgumentException], () => p1.loseGem())
     assertEquals("requirement failed: the gemCounter must be non-negative.", e1.getMessage)
-    assertEquals(p1.deck, List())
+    assertEquals(p1.deck, ListBuffer())
     assertEquals(p2.deck, d2)
-    assertEquals(p1.hand, List())
+    assertEquals(p1.hand, ListBuffer())
     assertEquals(p2.hand, h2)
   }
 
   test("deck setter") {
     p1.deck_=(d1)
-    assertEquals(p1.deck, List[Card](cc1, rc1, wc1))
+    assertEquals(p1.deck, ListBuffer[Card](cc1, rc1, wc1))
   }
 
   test("well defined hand and deck") {
     p2.shuffleDeck()
-    p2.deck == d2
+    assertEquals(p2.deck.length, d2.length)
+    assertEquals(p2.hand, h2)
 
     val d = p2.deck.tail
     p2.drawCard()
-
     assertEquals(p2.deck, d)
-    assertEquals(h2.length+1, p2.hand.length)
-    println(d2)
+    assertEquals(p2.hand.length, h2.length+1)
   }
 
   test("playCard") {
@@ -88,8 +90,8 @@ class PlayerTest extends munit.FunSuite {
   test("equals") {
     assertEquals(p1, p1)
     assertEquals(p2, p2)
-    assertEquals(p1, new Player("player1", s1))
-    assertEquals(p2, new Player("player2", s1, _deck = d2, _hand = h2))
+    assertEquals(p1, new Player("player1"))
+    assertEquals(p2, new Player("player2", d2, h2))
 
     assert(!p1.equals(cc1))
   }
