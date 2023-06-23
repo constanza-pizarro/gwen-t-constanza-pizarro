f406e79d3ebfaffb02d1f4cce07ba864a68e4974
Constanza Pizarro <constanza.pizarro.o@ug.uchile.cl>
2023-06-23 12:43:52 -0400

    TEST: Adds 'morale boost' test in UnitEffectTest

--- src/test/scala/gwent/cards/effects/UnitEffectTest.scala
+++ src/test/scala/gwent/cards/effects/UnitEffectTest.scala
@@ -11,13 +11,15 @@ class UnitEffectTest extends munit.FunSuite {
   val mBoost = "Adds +1 to all units in the row (excluding itself)."
   val tBond = "When placed with the same card, doubles the strength of both (or more) cards"
 
-  val cc1: Card = new CloseCombatCard("Blue Stripes Commando", TightBond(), tBond, 4)
-  val cc2: Card = new CloseCombatCard("Blueboy Lugos", NoEffect(), "Has no effect.", 6)
+  val cc1 = new CloseCombatCard("Blue Stripes Commando", TightBond(), tBond, 4)
+  val cc2 = new CloseCombatCard("Blueboy Lugos", NoEffect(), "Has no effect.", 6)
 
-  val rc1: Card = new RangedCombatCard("Albrich", NoEffect(), "Has no effect.", 2)
-  val rc2: Card = new RangedCombatCard("Milva", MoraleBoost(), mBoost, 10)
+  val rc1 = new RangedCombatCard("Albrich", NoEffect(), "Has no effect.", 2)
+  val rc2 = new RangedCombatCard("Milva", MoraleBoost(), mBoost, 10)
+  val rc3 = new RangedCombatCard("Cynthia", NoEffect(), "Has no effect.", 4)
+  val rc4 = new RangedCombatCard("Milva", MoraleBoost(), mBoost, 10)
 
-  val sc1: Card = new SiegeCombatCard("Ballista", NoEffect(), "Does nothing c:", 6)
+  val sc1 = new SiegeCombatCard("Ballista", NoEffect(), "Does nothing c:", 6)
   val sc2 = new SiegeCombatCard("Catapult", TightBond(), tBond, 8)
   val sc3 = new SiegeCombatCard("Catapult", TightBond(), tBond, 8)
 
@@ -27,16 +29,28 @@ class UnitEffectTest extends munit.FunSuite {
     "Sets the strength of all Ranged Combat cards to 1 for both players.")
 
   val d1: List[Card] = List(cc1, cc2, rc1, sc1, wc1)
-  val d2: List[Card] = List(cc2, rc1, rc2, sc1, wc2)
+  val d2: List[Card] = List(cc2, sc2, sc3, wc1, wc2)
 
   val h1: List[Card] = List(rc2, sc2, sc3, wc2)
-  val h2: List[Card] = List(cc1, wc1, sc2)
-  val s: Section = new Section()
+  val h2: List[Card] = List(rc1, rc2, rc3, sc1, rc4)
 
   val p1 = new Player("player1", _deck = d1, _hand = h1)
   val p2 = new Player("player2", _deck = d2, _hand = h2)
   val p3 = new Player("player3", _deck = d1, _hand = h1)
   val board = new Board(p1, p2)
+
+  test("morale boost") {
+    board.playCard(p2, rc1)
+    assertEquals(rc1.currentPower, rc1.power)
+    board.playCard(p2, rc2)
+    assertEquals(rc1.currentPower, rc1.power+1)
+    board.playCard(p2, rc3)
+    assertEquals(rc3.currentPower, rc3.power)
+    board.playCard(p2, rc4) // le sube 1 a rc1, rc2 y rc3
+    assertEquals(rc1.currentPower, rc1.power+2)
+    assertEquals(rc2.currentPower, rc2.power+1)
+    assertEquals(rc3.currentPower, rc3.power+1)
+  }
 
   test("tight bond") {
     board.playCard(p1, sc2)
