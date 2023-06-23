5b2ae22e99183bc844a9b1db81f8e373e7bac12e
Constanza Pizarro <constanza.pizarro.o@ug.uchile.cl>
2023-06-23 15:00:39 -0400

    TEST: Modifies parameters in BoardTest

--- src/test/scala/gwent/BoardTest.scala
+++ src/test/scala/gwent/BoardTest.scala
@@ -2,9 +2,11 @@ package cl.uchile.dcc
 package gwent
 
 import gwent.cards.*
-import cl.uchile.dcc.gwent.cards.effects.unit.*
-import cl.uchile.dcc.gwent.cards.effects.weather.*
+import gwent.cards.effects.unit.*
+import gwent.cards.effects.weather.*
+
 import org.junit.Assert
+import scala.collection.mutable.ListBuffer
 
 class BoardTest extends munit.FunSuite {
   val sc1 = new SiegeCombatCard("Ballista", NoEffect(),
@@ -14,16 +16,16 @@ class BoardTest extends munit.FunSuite {
   val wc2 = new WeatherCard("Impenetrable Fog", ImpenetrableFog(),
     "Sets the strength of all Ranged Combat cards to 1 for both players.")
 
-  val d1: List[Card] = List(wc1)
-  val d2: List[Card] = List(sc1, wc2)
+  val d1: ListBuffer[Card] = ListBuffer(wc1)
+  val d2: ListBuffer[Card] = ListBuffer(sc1, wc2)
 
-  val h1: List[Card] = List(sc1, wc2)
-  val h2: List[Card] = List(wc1)
-  val s: Section = new Section()
+  val h1: ListBuffer[Card] = ListBuffer(sc1, wc2)
+  val h2: ListBuffer[Card] = ListBuffer(wc1)
+  val s: Section = new Section
 
-  val p1 = new Player("player1", _deck = d1, _hand = h1)
-  val p2 = new Player("player2", _deck = d2, _hand = h2)
-  val p3 = new Player("player3", _deck = d1, _hand = h1)
+  val p1 = new Player("player1", d1, h1)
+  val p2 = new Player("player2", d2, h2)
+  val p3 = new Player("player3", d1, h1)
   val board = new Board(p1, p2)
 
   test("well defined board") {
