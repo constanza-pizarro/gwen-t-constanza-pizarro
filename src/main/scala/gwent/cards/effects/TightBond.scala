9ecefb9116df2bc357c3181bd6c39bbe1a61001f
Constanza Pizarro <constanza.pizarro.o@ug.uchile.cl>
2023-06-22 23:30:13 -0400

    FEAT: Adds apply method in TightBond

--- src/main/scala/gwent/cards/effects/TightBond.scala
+++ src/main/scala/gwent/cards/effects/TightBond.scala
@@ -1,6 +1,14 @@
 package cl.uchile.dcc
 package gwent.cards.effects
 
-class TightBond {
+import gwent.cards.UnitCard
 
+class TightBond extends UnitEffect {
+  def apply(self: UnitCard, target: List[UnitCard]): Unit = {
+    val nTarget = target.filter(_.equals(self))
+    if (nTarget.nonEmpty) {
+      nTarget.foreach(_.currentPower *= 2)
+      self.currentPower *= 2
+    }
+  }
 }
