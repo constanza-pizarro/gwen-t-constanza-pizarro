996026c41f649fe4bbdcf70ff0398703f143aa52
Constanza Pizarro <constanza.pizarro.o@ug.uchile.cl>
2023-06-24 13:07:44 -0400

    REFACTOR: Modifies override of apply(Card, Board) method in AbstractWeatherEffect

--- src/main/scala/gwent/cards/effects/AbstractWeatherEffect.scala
+++ src/main/scala/gwent/cards/effects/AbstractWeatherEffect.scala
@@ -1,11 +1,14 @@
 package cl.uchile.dcc
 package gwent.cards.effects
 
-import gwent.cards.*
+import gwent.cards.{Card, UnitCard}
+import gwent.Board
 
 abstract class AbstractWeatherEffect extends Effect {
   override def apply(self: Card, target: List[UnitCard]): Unit = {
     // do nothing :p
   }
-
+  override def apply(self: Card, target: Board): Unit = {
+    target.applied += this
+  }
 }
