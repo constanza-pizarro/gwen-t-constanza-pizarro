c7439fd4460dedd983b5c1674ace800848b7a224
Constanza Pizarro <constanza.pizarro.o@ug.uchile.cl>
2023-06-23 23:45:55 -0400

    TEST: Modifies 'changeTurn' test in GameControllerTest

--- src/test/scala/gwent/controller/GameControllerTest.scala
+++ src/test/scala/gwent/controller/GameControllerTest.scala
@@ -37,6 +37,8 @@ class GameControllerTest extends munit.FunSuite {
     gameC.changeTurn()
     assertEquals(gameC.otherPlayer.get, cPlayer)
     assertEquals(gameC.currentPlayer.get, oPlayer)
+    val e3 = Assert.assertThrows(classOf[InvalidTransitionException], () => gameC.state.newRound())
+    assertEquals(s"Cannot transition from TurnState to RoundState", e3.getMessage)
   }
 
   test("turn state") {
@@ -48,15 +50,16 @@ class GameControllerTest extends munit.FunSuite {
     gameC.endTurn()
     assert(gameC.isInAlone)
     assert(!gameC2.isInAlone)
-    val e3 = Assert.assertThrows(classOf[InvalidTransitionException], () => gameC.state.startGame())
-    assertEquals(s"Cannot transition from AloneState to TurnState", e3.getMessage)
+    val e4 = Assert.assertThrows(classOf[InvalidTransitionException], () => gameC.state.startGame())
+    assertEquals(s"Cannot transition from AloneState to TurnState", e4.getMessage)
+
   }
 
   test("playCard") {
     val player: Player = gameC.currentPlayer.get
     val i: Int = player.hand.length
-    val e4 = Assert.assertThrows(classOf[InvalidNumberException], () => gameC.playCard(i+1))
-    assertEquals(s"The number must be less than $i.", e4.getMessage)
+    val e5 = Assert.assertThrows(classOf[InvalidNumberException], () => gameC.playCard(i+1))
+    assertEquals(s"The number must be less than $i.", e5.getMessage)
     gameC.playCard(0)
     assertEquals(gameC.otherPlayer.get, player)
   }
