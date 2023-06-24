a09cfaf4dde2757a3618adf70857784567024097
Constanza Pizarro <constanza.pizarro.o@ug.uchile.cl>
2023-06-24 01:28:25 -0400

    REFACTOR: Modifies apply method in MoraleBoost

--- src/main/scala/gwent/cards/effects/unit/MoraleBoost.scala
+++ src/main/scala/gwent/cards/effects/unit/MoraleBoost.scala
@@ -1,11 +1,11 @@
 package cl.uchile.dcc
 package gwent.cards.effects.unit
 
-import gwent.cards.effects.UnitEffect
-import gwent.cards.UnitCard
+import gwent.cards.effects.AbstractUnitEffect
+import gwent.cards.*
 
-case class MoraleBoost() extends UnitEffect {
-  def apply(self: UnitCard, target: List[UnitCard]): Unit = {
+case class MoraleBoost() extends AbstractUnitEffect {
+  override def apply(self: Card, target: List[UnitCard]): Unit = {
     var nTarget = target.filterNot(_ eq self)
     nTarget.foreach(i => i.currentPower += 1)
   }
