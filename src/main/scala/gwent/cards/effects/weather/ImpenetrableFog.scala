e85605ead030fd3245a0b4a83232895614cfa9f6
Constanza Pizarro <constanza.pizarro.o@ug.uchile.cl>
2023-06-24 13:12:05 -0400

    FEAT: Overrides apply(Card, Board) method in ImpenetrableFog

--- src/main/scala/gwent/cards/effects/weather/ImpenetrableFog.scala
+++ src/main/scala/gwent/cards/effects/weather/ImpenetrableFog.scala
@@ -1,10 +1,22 @@
 package cl.uchile.dcc
 package gwent.cards.effects.weather
 
-import gwent.cards.effects.WeatherEffect
+import gwent.cards.effects.AbstractWeatherEffect
+import cl.uchile.dcc.gwent.*
+import cl.uchile.dcc.gwent.cards.Card
 
-case class ImpenetrableFog() extends WeatherEffect {
-  def apply(): Unit = {
-
+case class ImpenetrableFog() extends AbstractWeatherEffect {
+  override def apply(self: Card, target: Board): Unit = {
+    super.apply(self, target)
+    target.player1.section.rangedCombatZone
+      .foreach(card => {
+        card.lastPower = card.currentPower
+        card.currentPower = 1
+      })
+    target.player2.section.rangedCombatZone
+      .foreach(card => {
+        card.lastPower = card.currentPower
+        card.currentPower = 1
+      })
   }
 }
