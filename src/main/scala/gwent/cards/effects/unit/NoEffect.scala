49420a2068fe55bd0b84e2040769bdc94fe14a8f
Constanza Pizarro <constanza.pizarro.o@ug.uchile.cl>
2023-06-24 01:29:01 -0400

    REFACTOR: Modifies apply method in NoEffect

--- src/main/scala/gwent/cards/effects/unit/NoEffect.scala
+++ src/main/scala/gwent/cards/effects/unit/NoEffect.scala
@@ -1,11 +1,11 @@
 package cl.uchile.dcc
 package gwent.cards.effects.unit
 
-import gwent.cards.effects.UnitEffect
-import gwent.cards.UnitCard
+import gwent.cards.effects.AbstractUnitEffect
+import gwent.cards.*
 
-case class NoEffect() extends UnitEffect {
-  def apply(self: UnitCard, target: List[UnitCard]): Unit = {
+case class NoEffect() extends AbstractUnitEffect {
+  override def apply(self: Card, target: List[UnitCard]): Unit = {
     // do nothing :P
   }
 }
