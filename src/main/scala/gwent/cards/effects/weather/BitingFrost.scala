0bbe3b56eeb0cd47c4eae92827fb8cde53ca72c7
Constanza Pizarro <constanza.pizarro.o@ug.uchile.cl>
2023-06-24 13:10:28 -0400

    FEAT: Overrides apply(Card, Board) method in BitingFrost

--- src/main/scala/gwent/cards/effects/weather/BitingFrost.scala
+++ src/main/scala/gwent/cards/effects/weather/BitingFrost.scala
@@ -1,10 +1,22 @@
 package cl.uchile.dcc
 package gwent.cards.effects.weather
 
-import gwent.cards.effects.WeatherEffect
+import gwent.cards.effects.AbstractWeatherEffect
+import gwent.cards.*
+import gwent.*
 
-case class BitingFrost() extends WeatherEffect {
-  def apply(): Unit = {
-
+case class BitingFrost() extends AbstractWeatherEffect {
+  override def apply(self: Card, target: Board): Unit = {
+    super.apply(self, target)
+    target.player1.section.closeCombatZone
+      .foreach(card => {
+        card.lastPower = card.currentPower
+        card.currentPower = 1
+      })
+    target.player2.section.closeCombatZone
+      .foreach(card => {
+        card.lastPower = card.currentPower
+        card.currentPower = 1
+      })
   }
 }
