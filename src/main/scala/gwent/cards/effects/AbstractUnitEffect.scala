4e87227ecb8f5317469db123816e2ebf4fe60250
Constanza Pizarro <constanza.pizarro.o@ug.uchile.cl>
2023-06-24 01:25:38 -0400

    FEAT: Implements apply method in AbstractUnitEffect

--- src/main/scala/gwent/cards/effects/AbstractUnitEffect.scala
+++ src/main/scala/gwent/cards/effects/AbstractUnitEffect.scala
@@ -1,8 +1,11 @@
 package cl.uchile.dcc
 package gwent.cards.effects
 
-import gwent.cards.UnitCard
+import gwent.cards.Card
+import gwent.Section
 
 abstract class AbstractUnitEffect extends Effect {
-
+  override def apply(self: Card, section1: Section, section2: Section): Unit = {
+    // do nothing c:
+  }
 }
