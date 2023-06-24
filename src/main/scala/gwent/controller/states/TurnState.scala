2514e5eb043a1267ed40db7ce57a951f97497704
Constanza Pizarro <constanza.pizarro.o@ug.uchile.cl>
2023-06-23 20:47:47 -0400

    REFACTOR: Modifies endTurn method in TurnState

--- src/main/scala/gwent/controller/states/TurnState.scala
+++ src/main/scala/gwent/controller/states/TurnState.scala
@@ -5,6 +5,7 @@ import gwent.controller.GameController
 
 class TurnState(controller: GameController) extends GameState(controller) {
   override def endTurn(): Unit = {
+    controller.changeTurn()
     controller.state = new AloneState(controller)
   }
   override def isInTurn: Boolean = true
