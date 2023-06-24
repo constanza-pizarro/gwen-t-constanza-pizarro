28cb45dd5509d71b2b27e22cea6f5bccae53c6a4
Constanza Pizarro <constanza.pizarro.o@ug.uchile.cl>
2023-06-24 13:45:08 -0400

    REFACTOR: Modifies CountState

--- src/main/scala/gwent/controller/states/CountState.scala
+++ src/main/scala/gwent/controller/states/CountState.scala
@@ -6,10 +6,9 @@ import gwent.controller.GameController
 import cl.uchile.dcc.gwent.Player
 
 class CountState(controller: GameController) extends GameState(controller) {
-  /*
   override def newRound(): Unit = {
     controller.state = new RoundState(controller)
-  }*/
+  }
   override def declareWinner(): Unit = {
     controller.state = new FinalState(controller)
   }
