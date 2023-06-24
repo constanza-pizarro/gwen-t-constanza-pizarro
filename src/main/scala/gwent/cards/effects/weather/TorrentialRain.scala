c9eb10e4deaee5b2eeaf132d8b36399cd9c4eeb6
Constanza Pizarro <constanza.pizarro.o@ug.uchile.cl>
2023-06-24 13:13:13 -0400

    FEAT: Overrides apply(Card, Board) method in TorrentialRain

--- src/main/scala/gwent/cards/effects/weather/TorrentialRain.scala
+++ src/main/scala/gwent/cards/effects/weather/TorrentialRain.scala
@@ -1,10 +1,23 @@
 package cl.uchile.dcc
 package gwent.cards.effects.weather
 
-import gwent.cards.effects.WeatherEffect
+import gwent.cards.effects.AbstractWeatherEffect
 
-case class TorrentialRain() extends WeatherEffect {
-  def apply(): Unit = {
+import cl.uchile.dcc.gwent.*
+import cl.uchile.dcc.gwent.cards.Card
 
+case class TorrentialRain() extends AbstractWeatherEffect {
+  override def apply(self: Card, target: Board): Unit = {
+    super.apply(self, target)
+    target.player1.section.siegeCombatZone
+      .foreach(card => {
+        card.lastPower = card.currentPower
+        card.currentPower = 1
+      })
+    target.player2.section.siegeCombatZone
+      .foreach(card => {
+        card.lastPower = card.currentPower
+        card.currentPower = 1
+      })
   }
 }
