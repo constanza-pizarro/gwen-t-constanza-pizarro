ecddd91c7ce27668fff90ef20d5553f9056955ea
Constanza Pizarro <constanza.pizarro.o@ug.uchile.cl>
2023-06-24 13:06:10 -0400

    REFACTOR: Modifies apply method signature and adds undo method in Effect

--- src/main/scala/gwent/cards/effects/Effect.scala
+++ src/main/scala/gwent/cards/effects/Effect.scala
@@ -2,9 +2,12 @@ package cl.uchile.dcc
 package gwent.cards.effects
 
 import gwent.cards.*
-import gwent.Section
+import gwent.*
 
 trait Effect {
   def apply(self: Card, target: List[UnitCard]): Unit
-  def apply(self: Card, section1: Section, section2: Section): Unit
+  def apply(self: Card, target: Board): Unit
+  def undo(target: Board): Unit = {
+    // do nothing uwu
+  }
 }
