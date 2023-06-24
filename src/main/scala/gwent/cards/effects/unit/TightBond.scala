2c9397f9b02e95cdf5d4c3c0d34b65e9753a5c9e
Constanza Pizarro <constanza.pizarro.o@ug.uchile.cl>
2023-06-24 01:29:31 -0400

    REFACTOR: Modifies apply method in TightBond

--- src/main/scala/gwent/cards/effects/unit/TightBond.scala
+++ src/main/scala/gwent/cards/effects/unit/TightBond.scala
@@ -1,11 +1,11 @@
 package cl.uchile.dcc
 package gwent.cards.effects.unit
 
-import gwent.cards.effects.UnitEffect
-import gwent.cards.UnitCard
+import gwent.cards.effects.AbstractUnitEffect
+import gwent.cards.*
 
-case class TightBond() extends UnitEffect {
-  def apply(self: UnitCard, target: List[UnitCard]): Unit = {
+case class TightBond() extends AbstractUnitEffect {
+  override def apply(self: Card, target: List[UnitCard]): Unit = {
     val nTarget = target.filter(_.equals(self))
     if (nTarget.length>1)
       nTarget.foreach(_.currentPower *=2)
