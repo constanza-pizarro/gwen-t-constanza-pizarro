ade8744e110659cbbeb1a9dac1c51dafc3d421b1
Constanza Pizarro <constanza.pizarro.o@ug.uchile.cl>
2023-06-24 01:12:36 -0400

    Overloads apply method with different arity in Effect

--- src/main/scala/gwent/cards/effects/Effect.scala
+++ src/main/scala/gwent/cards/effects/Effect.scala
@@ -1,5 +1,10 @@
-package cl.uchile.dcc.gwent.cards.effects
+package cl.uchile.dcc
+package gwent.cards.effects
+
+import gwent.cards.*
+import gwent.Section
 
 trait Effect {
-
+  def apply(self: Card, target: List[UnitCard]): Unit
+  def apply(self: Card, section1: Section, section2: Section): Unit
 }
