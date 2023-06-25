2d569f50a6f0ab3e660e7254b2a8af09bf35d5c2
Constanza Pizarro <constanza.pizarro.o@ug.uchile.cl>
2023-06-25 19:50:05 -0400

    REFACTOR: Modifies apply and undo methods in weather effects

--- src/main/scala/gwent/cards/effects/weather/BitingFrost.scala
+++ src/main/scala/gwent/cards/effects/weather/BitingFrost.scala
@@ -8,27 +8,9 @@ import gwent.*
 case class BitingFrost() extends AbstractWeatherEffect {
   override def apply(self: Card, target: Board): Unit = {
     super.apply(self, target)
-    target.player1.section.closeCombatZone
-      .foreach(card => {
-        card.lastPower = card.currentPower
-        card.currentPower = 1
-      })
-    target.player2.section.closeCombatZone
-      .foreach(card => {
-        card.lastPower = card.currentPower
-        card.currentPower = 1
-      })
+    applyEffect(target.player1.section.closeCombatZone, target.player2.section.closeCombatZone)
   }
   override def undo(target: Board): Unit = {
-    target.player1.section.closeCombatZone
-      .foreach(card => {
-        val lp = card.lastPower
-        if (lp > 0) card.currentPower = lp
-      })
-    target.player2.section.closeCombatZone
-      .foreach(card => {
-        val lp = card.lastPower
-        if (lp > 0) card.currentPower = lp
-      })
+    unapplyEffect(target.player1.section.closeCombatZone, target.player2.section.closeCombatZone)
   }
 }
--- src/main/scala/gwent/cards/effects/weather/ImpenetrableFog.scala
+++ src/main/scala/gwent/cards/effects/weather/ImpenetrableFog.scala
@@ -8,27 +8,9 @@ import cl.uchile.dcc.gwent.cards.Card
 case class ImpenetrableFog() extends AbstractWeatherEffect {
   override def apply(self: Card, target: Board): Unit = {
     super.apply(self, target)
-    target.player1.section.rangedCombatZone
-      .foreach(card => {
-        card.lastPower = card.currentPower
-        card.currentPower = 1
-      })
-    target.player2.section.rangedCombatZone
-      .foreach(card => {
-        card.lastPower = card.currentPower
-        card.currentPower = 1
-      })
+    applyEffect(target.player1.section.rangedCombatZone, target.player2.section.rangedCombatZone)
   }
   override def undo(target: Board): Unit = {
-    target.player1.section.rangedCombatZone
-      .foreach(card => {
-        val lp = card.lastPower
-        if (lp > 0) card.currentPower = lp
-      })
-    target.player2.section.rangedCombatZone
-      .foreach(card => {
-        val lp = card.lastPower
-        if (lp > 0) card.currentPower = lp
-      })
+    unapplyEffect(target.player1.section.rangedCombatZone, target.player2.section.rangedCombatZone)
   }
 }
