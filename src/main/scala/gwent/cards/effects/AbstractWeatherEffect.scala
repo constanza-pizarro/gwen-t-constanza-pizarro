f4e78158a5070d452f154a3b8532534be5ddd68e
Constanza Pizarro <constanza.pizarro.o@ug.uchile.cl>
2023-06-25 19:49:16 -0400

    FEAT: Adds applyEffect and unapplyEffect methods in AbstractWeatherEffect

--- src/main/scala/gwent/cards/effects/AbstractWeatherEffect.scala
+++ src/main/scala/gwent/cards/effects/AbstractWeatherEffect.scala
@@ -11,4 +11,28 @@ abstract class AbstractWeatherEffect extends Effect {
   override def apply(self: Card, target: Board): Unit = {
     target.applied += this
   }
+  def applyEffect(list1: List[Card], list2: List[Card]): Unit = {
+    list1
+      .foreach(card => {
+        card.lastPower = card.currentPower
+        card.currentPower = 1
+      })
+    list2
+      .foreach(card => {
+        card.lastPower = card.currentPower
+        card.currentPower = 1
+      })
+  }
+  def unapplyEffect(list1: List[Card], list2: List[Card]): Unit = {
+    list1
+      .foreach(card => {
+        val lp = card.lastPower
+        if (lp > 0) card.currentPower = lp
+      })
+    list2
+      .foreach(card => {
+        val lp = card.lastPower
+        if (lp > 0) card.currentPower = lp
+      })
+  }
 }
