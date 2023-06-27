6d2e55fbe32ff2b5eb07ee42575dd0636e00ba92
Constanza Pizarro <constanza.pizarro.o@ug.uchile.cl>
2023-06-27 19:43:23 -0400

    DOCS: Adds documentation in TightBond

--- src/main/scala/gwent/cards/effects/unit/TightBond.scala
+++ src/main/scala/gwent/cards/effects/unit/TightBond.scala
@@ -4,6 +4,17 @@ package gwent.cards.effects.unit
 import gwent.cards.effects.AbstractUnitEffect
 import gwent.cards.*
 
+/** Class representing the tight bond effect of an unit card in the Gwen't game.
+ *
+ * A `TightBond` is a type of [[AbstractUnitEffect]].
+ * As per the game's rules, when a card with this effect is placed on the board 
+ * if the same card was already on the board (at least once) the current power of 
+ * all these cards is duplicated.
+ *
+ * @constructor Creates a new `TightBond` effect.
+ *
+ * @author Constanza Pizarro
+ */
 case class TightBond() extends AbstractUnitEffect {
   override def apply(self: Card, target: List[UnitCard]): Unit = {
     val nTarget = target.filter(_.equals(self))
