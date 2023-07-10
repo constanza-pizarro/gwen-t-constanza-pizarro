60ee98d1dfbeca9ea2a41080ac2e9ec8220c74cd
Constanza Pizarro <constanza.pizarro.o@ug.uchile.cl>
2023-07-10 15:19:02 -0400

    DOCS: Adds documentation to AbstractUnitEffect

--- src/main/scala/gwent/cards/effects/AbstractUnitEffect.scala
+++ src/main/scala/gwent/cards/effects/AbstractUnitEffect.scala
@@ -4,6 +4,15 @@ package gwent.cards.effects
 import gwent.cards.Card
 import gwent.Board
 
+/** Abstract class representing an effect of a unit card in the Gwen't game.
+ *
+ * An `AbstractUnitEffect` is a type of [[Effect]] that can be applied to a
+ * specific unit zone of a board.
+ *
+ * @constructor Creates a new `AbstractUnitEffect`.
+ *
+ * @author Constanza Pizarro
+ */
 abstract class AbstractUnitEffect extends Effect {
   override def apply(self: Card, board: Board): Unit = {
     // do nothing c:
