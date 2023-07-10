4adae7eb877bd0321924e011e49635d70c9bf1ca
Constanza Pizarro <constanza.pizarro.o@ug.uchile.cl>
2023-07-10 15:15:47 -0400

    DOCS: Modifies documentation in Effect

--- src/main/scala/gwent/cards/effects/Effect.scala
+++ src/main/scala/gwent/cards/effects/Effect.scala
@@ -8,11 +8,22 @@ import gwent.*
  * Represents the common structure of an effect of a card in the game.
  */
 trait Effect {
-  /** Applies the effect of the card to an unit section of the board. */
+  /** Applies the effect of a card to an unit zone of the board.
+   *
+   * @param self the card whose effect is applied
+   * @param target the unit zone where the effect is applied
+   */
   def apply(self: Card, target: List[UnitCard]): Unit
-  /** Applies the effect of the card to the weather section of the board. */
+  /** Applies the effect of a card to an unit zone of both players of the board.
+   *
+   * @param self the card whose effect is applied
+   * @param target the board where the effect is applied
+   */
   def apply(self: Card, target: Board): Unit
-  /** Reverses the current weather cards' effects. */
+  /** Reverses the current weather cards' effects.
+   *
+   * @param target the board where the current weather effects are reverted
+   */
   def undo(target: Board): Unit = {
     // do nothing uwu
   }
