f3f2c896af3b76854de73fff951fe2338c4dd0c5
Constanza Pizarro <constanza.pizarro.o@ug.uchile.cl>
2023-07-10 15:28:16 -0400

    DOCS: Adds documentation to AbstractWeatherEffect

--- src/main/scala/gwent/cards/effects/AbstractWeatherEffect.scala
+++ src/main/scala/gwent/cards/effects/AbstractWeatherEffect.scala
@@ -4,6 +4,15 @@ package gwent.cards.effects
 import gwent.cards.{Card, UnitCard}
 import gwent.Board
 
+/** Abstract class representing an effect of a weather card in the Gwen't game.
+ *
+ * An `AbstractWeatherEffect` is a type of [[Effect]] that can be applied to a
+ * specific unit zone of both players a board.
+ *
+ * @constructor Creates a new `AbstractWeatherEffect`.
+ *
+ * @author Constanza Pizarro
+ */
 abstract class AbstractWeatherEffect extends Effect {
   override def apply(self: Card, target: List[UnitCard]): Unit = {
     // do nothing :p
@@ -11,6 +20,14 @@ abstract class AbstractWeatherEffect extends Effect {
   override def apply(self: Card, target: Board): Unit = {
     target.applied += this
   }
+  /** Applies a weather effect to two lists of unit cards.
+   *
+   * Saves the previous power and sets the current power to 1 of all
+   * the cards of both lists.
+   *
+   * @param list1 a list of unit cards
+   * @param list2 a list of unit cards
+   */
   def applyEffect(list1: List[UnitCard], list2: List[UnitCard]): Unit = {
     list1
       .foreach(card => {
@@ -23,6 +40,14 @@ abstract class AbstractWeatherEffect extends Effect {
         card.currentPower = 1
       })
   }
+  /** Reverses all weather effects to two lists of unit cards.
+   *
+   * Sets the current power to the previous power of all cards
+   * (of both lists) whose previous power was modified.
+   *
+   * @param list1 a list of unit cards
+   * @param list2 a list of unit cards
+   */
   def unapplyEffect(list1: List[UnitCard], list2: List[UnitCard]): Unit = {
     list1
       .foreach(card => {
