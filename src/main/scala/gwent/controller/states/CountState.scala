14308533d830179fcf2f800aafd403b744c50445
Constanza Pizarro <constanza.pizarro.o@ug.uchile.cl>
2023-07-17 11:28:50 -0400

    REFACTOR: Removes declareWinner method in CountState

--- src/main/scala/gwent/controller/states/CountState.scala
+++ src/main/scala/gwent/controller/states/CountState.scala
@@ -9,8 +9,5 @@ class CountState(controller: GameController) extends GameState(controller) {
   override def newRound(): Unit = {
     controller.state = new RoundState(controller)
   }
-  override def declareWinner(): Unit = {
-    controller.state = new FinalState(controller)
-  }
   override def isInCount: Boolean = true
 }
