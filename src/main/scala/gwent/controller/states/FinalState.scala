dd4b823584dd2476b20f38b3ab793a1ab95a816c
Constanza Pizarro <constanza.pizarro.o@ug.uchile.cl>
2023-07-19 11:27:48 -0400

    FEAT: Overrides playAgain method in FinalState

--- src/main/scala/gwent/controller/states/FinalState.scala
+++ src/main/scala/gwent/controller/states/FinalState.scala
@@ -7,5 +7,8 @@ class FinalState(controller: GameController) extends GameState(controller) {
   override def declareWinner(): Unit = {
     // do nothing
   }
+  override def playAgain(): Unit = {
+    controller.state = new StartState(controller)
+  }
   override def isInFinal: Boolean = true
 }
