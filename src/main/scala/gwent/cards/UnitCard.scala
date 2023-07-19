82068a2ab3611992e38b8c136f79328e96e3857e
Constanza Pizarro <constanza.pizarro.o@ug.uchile.cl>
2023-07-19 14:48:57 -0400

    DOCS: Modifies documentation in UnitCard

--- src/main/scala/gwent/cards/UnitCard.scala
+++ src/main/scala/gwent/cards/UnitCard.scala
@@ -4,6 +4,14 @@ package gwent.cards
 import gwent.Section
 import gwent.cards.effects.Effect
 
+/**
+ * Represents the common structure of a unit card in the game.
+ *
+ * A `UnitCard` is a type of [[Card]].
+ * A unit card is characterized by its [[name]], [[description]], [[effect]] and [[power]].
+ * It also has a [[currentPower]] and a [[previousPower]] to keep track of the effects
+ * applied to the card.
+ */
 trait UnitCard extends Card {
   val power: Int
   /** The current power of the card, which may be affected by various conditions during
@@ -11,6 +19,10 @@ trait UnitCard extends Card {
    * Initially set to the base [[power]] of the card.
    */
   var currentPower: Int = power
+  /** The previous power of the card, which may be changed when a weather card is placed
+   * on the board.
+   * Initially set to 0.
+   */
   var previousPower: Int = 0
   /** Puts the unit card in its respective section of the board.
    *
